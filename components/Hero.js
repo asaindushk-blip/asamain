647c2e31a1ba89a42c47d972e5b10dcc4ae9cee5
asaindushk-blip <asaindushk@gmail.com>
2025-08-12 16:23:40 +0530

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -9,14 +9,11 @@ const Hero = () => {
         alt="ASA Industries Hero Image"
         className="opacity-30 filter brightness-75 drop-shadow-md saturate-60"
       ></img>
-      <h3
-        className="absolute mx-auto text-6xl font-medium text-left text-white md:w-3/5 md:top-72 top-60"
-        style={{ left: "5%" }}
-      >
-
+          <h1 class="text-5xl md:text-7xl font-bold text-white mb-6 leading-tight animate-fade-in">Precision Electrical Stamping<span class="block text-accent">Die Casted Rotor & Sheet Metal Components</span></h1>
+          <span class="block text-accent"> Components</span>
+          </h1>
+    
   
-        Precision Electrical Stamping Die Casted Rotors</br> Sheet Metal Components
-      </h3>
     </div>
   );
 };
