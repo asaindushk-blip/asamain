2c29a903f68c8f9d8aa3df92a81513f7f47d0e6e
asaindushk-blip <asaindushk@gmail.com>
2025-08-20 11:20:56 +0530

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -4,15 +4,17 @@ const Hero = () => {
       className="relative grid w-screen grid-rows-1 bg-[#000C56] lg:top-20 top-0 md:object-cover"
       id="Intro"
     >
-      <img src="hero.png" alt="ASA Industries Hero Image" class="opacity-40 filter brightness-75 drop-shadow-md saturate-60">
+      <img
+        src="hero.png"
+        alt="ASA Industries Hero Image"
+        className="opacity-40 filter brightness-75 drop-shadow-md saturate-60"
       ></img>
-          <h3 class="absolute mx-auto text-6xl font-medium text-left text-white md:w-3/5 md:top-72 top-60" style="left:5%">Delivering Excellence<br> Since 1985</h3>
-          Delivering Excellence
-            <br>
-             "Since 1985"
-            </h3>
-      
-  
+      <h3
+        className="absolute mx-auto text-6xl font-medium text-left text-white md:w-3/5 md:top-72 top-60"
+        style={{ left: "5%" }}
+      >
+        Delivering Excellence<br></br> Since 1985
+      </h3>
     </div>
   );
 };
